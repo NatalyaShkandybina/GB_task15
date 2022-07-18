5040c2669725b06442807849da0e3c25b5c237bc
Natalya Shkandybina <natalya.shkandybina@mail.ru>
2022-07-18 23:06:41 +0300

    info about task13

---
+++ task13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+
+if(number<100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+// if(number > 99 && number <1000)
+// {
+//     Console.WriteLine(number % 10);
+// }
+else{
+    string nuber1 = number.ToString();
+    Console.WriteLine(nuber1[2]);
+}
