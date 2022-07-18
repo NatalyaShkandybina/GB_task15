a046a27c63affe7f9e042fbf4be3e6cdc1e26671
NatalyaShkandybina <109097854+NatalyaShkandybina@users.noreply.github.com>
2022-07-18 23:14:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("ВВедите число дня недели: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+switch (num1)
+{
+    case 1:
+        Console.WriteLine("Понедельник. Нет");
+        break;
+    case 2:
+        Console.WriteLine("Вторник. Нет");
+        break;
+    case 3:
+        Console.WriteLine("Среда. Нет");
+        break;
+    case 4:
+        Console.WriteLine("Четверг. Нет");
+        break;
+    case 5:
+        Console.WriteLine("Пятница. Нет");
+        break;
+    case 6:
+        Console.WriteLine("Суббота. Да.");
+        break;
+    case 7:
+        Console.WriteLine("Воскресенье. Да");
+        break;
+    default:
+        Console.WriteLine("Нет такого дня");
+        break;
+}
